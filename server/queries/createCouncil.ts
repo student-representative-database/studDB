e3ee26ff7aae5a1d649362dbdc474f8c02c29226
Fredriko83 <folep02@student.lnu.se>
2017-04-22 16:53:39 +0200

    git trouble... new gitignore

--- server/queries/createCouncil.ts
+++
@@ -1,6 +0,0 @@
-import {CouncilModel} from "../model/model";
-
-export function createCouncil(facultyId: number, props: any,) {
-    props.facultyId = facultyId;
-    return CouncilModel.create(props);
-}
--- server/queries/findOneCouncil.ts
+++
@@ -1,9 +0,0 @@
-import {CouncilModel} from "../model/model";
-import {createCouncil} from "../model/council";
-
-
-export function findOneCouncil(councilId: number) {
-    console.log(councilId);
-    return CouncilModel.findById(councilId)
-        .then(createCouncil);
-}
