a5c5bee16c6b5cbe12c8080989248351e0053d0c
Pär Eriksson <pe222gq@student.lnu.se>
2017-05-02 14:12:01 +0200

    Adding interface for RegistryListItem options.

--- client/src/RegistryList.ts
+++ client/src/RegistryList.ts
@@ -14,30 +14,12 @@ export class RegistryList {
   private init() {
     this.element = document.createElement('div')
     this.element.classList.add('list-group')
-    this.element.appendChild(this.topLevelItem())
 
-    this.element.addEventListener('click', (event) => {
-      this.eventHandler(event)
+    const topLevel = new RegistryListItem({
+      title: 'Bläddra i registret',
+      icon: true
     })
-  }
 
-  private topLevelItem() {
-    const item = document.createElement('a')
-    item.classList.add('list-group-item')
-    const icon = document.createElement('span')
-    icon.classList.add('glyphicon', 'glyphicon-triangle-right')
-    icon.setAttribute('aria-hidden', 'true')
-
-    item.appendChild(icon)
-    item.appendChild(document.createTextNode("Bläddra i registret"))
-
-    return item
-  }
-
-  private eventHandler(event) {
-    if (event.target.classList.contains('glyphicon')) {
-      event.target.classList.toggle('glyphicon-triangle-right')
-      event.target.classList.toggle('glyphicon-triangle-bottom')
-    }
+    this.element.appendChild(topLevel.element)
   }
 }
--- client/src/RegistryListItem.ts
+++ client/src/RegistryListItem.ts
@@ -1,5 +1,46 @@
-export class RegistryListItem {
-  public icon: HTMLElement
-  public element: HTMLElement
+interface IListItem {
+  title: string,
+  icon: boolean
+}
 
+export class RegistryListItem {
+  public icon: HTMLElement = null
+  public element: HTMLElement
+  private options: IListItem
+
+  constructor(options: IListItem) {
+    this.options = options
+    this.init()
+  }
+
+  private init() {
+    const item = document.createElement('a')
+    item.classList.add('list-group-item')
+
+    item.appendChild(document.createTextNode(this.options.title))
+    item.addEventListener('click', (event) => {
+      this.eventHandler(event)
+    })
+
+    this.element = item
+
+    if (this.options.icon) {
+      this.createIcon()
+    }
+  }
+
+  private createIcon() {
+    this.icon = document.createElement('span')
+    this.icon.classList.add('glyphicon', 'glyphicon-triangle-right')
+    this.icon.setAttribute('aria-hidden', 'true')
+
+    this.element.insertBefore(this.icon, this.element.firstChild)
+  }
+
+  private eventHandler(event) {
+    if (this.icon) {
+      this.icon.classList.toggle('glyphicon-triangle-right')
+      this.icon.classList.toggle('glyphicon-triangle-bottom')
+    }
+  }
 }
