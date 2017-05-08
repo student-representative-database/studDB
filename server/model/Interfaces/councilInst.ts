7ea96115f5cfae9f3b6a3856c579bfec71e603d7
AndrasBalla <ab223sq@student.lnu.se>
2017-05-08 14:23:44 +0200

    Fixed the routes

--- server/model/Interfaces/councilInst.ts
+++ server/model/Interfaces/councilInst.ts
@@ -9,6 +9,9 @@ export interface ICouncilInst {
 }
 
 export function createCouncilInstance({ id, councilId, from, till, Users }: any): ICouncilInst {
+    const today = new Date();
+    console.log(from.getFullYear());
+    console.log(today.getTime() < till.getTime() && today.getTime() > from.getTime());
     return {
         id,
         councilId,
