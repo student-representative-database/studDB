3ce74ffddf1865b14d96b607259f0bd2de1adf86
Fredriko83 <folep02@student.lnu.se>
2017-05-30 20:45:45 +0200

    getreplist

--- server/routes/web/admin/AdminStudentsRouter.ts
+++ server/routes/web/admin/AdminStudentsRouter.ts
@@ -1,47 +1,61 @@
-import { NextFunction, Request, Response, Router } from 'express'
+import {NextFunction, Request, Response, Router} from 'express'
 import DAO from '../../../utils/DAO'
 
 class AdminStudentsRouter {
-  public router: Router;
+    public router: Router;
 
-  constructor() {
-    this.router = Router();
-    this.init()
-  }
+    constructor() {
+        this.router = Router();
+        this.init()
+    }
 
-  public getStudents(req: Request, res: Response, next: NextFunction) {
-    DAO.getAllStudents()
-        .then((result) => {
-            res
-              .status(200)
-              .render('./admin/students', {students: result.payload , layout: 'admin'});
-        });
-  }
+    public getStudents(req: Request, res: Response, next: NextFunction) {
+        DAO.getAllStudents()
+            .then((result) => {
+                res
+                    .status(200)
+                    .render('./admin/students', {students: result.payload, layout: 'admin'});
+            });
+    }
 
-  public updateStudent(req: Request, res: Response, next: NextFunction) {
-    DAO.getOneStudent(req.params.id)
-        .then((result) => {
-          const data = result.payload
-          data.birthDate = result.payload.birthDate.substring(0, 10)
-          data.graduationYear = result.payload.graduationYear.substring(0, 4)
-          console.log(data)
-          res
+    public updateStudent(req: Request, res: Response, next: NextFunction) {
+        DAO.getOneStudent(req.params.id)
+            .then((result) => {
+                const data = result.payload
+                data.birthDate = result.payload.birthDate.substring(0, 10)
+                data.graduationYear = result.payload.graduationYear.substring(0, 4)
+                console.log(data)
+                res
+                    .status(200)
+                    .render('./admin/students', {student: result.payload, layout: 'admin'});
+            });
+    }
+
+    public getRepresentativesList(req: Request, res: Response, next: NextFunction) {
+
+        DAO.getAllFaculties()
+            .then((data) => {
+                    console.log(JSON.stringify(data, null, 2))
+                    for (let i = 0; i < data.payload.length; i++) {
+                        DAO.getAllCouncils(data.payload[i].id)
+                            .then((council) => {
+                                //console.log(JSON.stringify(council, null, 2));
+                                //data.payload[i]
+                            })
+                    }
+                }
+            )
+
+        res
             .status(200)
-            .render('./admin/students', {student: result.payload , layout: 'admin'});
-        });
-  }
+            .render('./admin/representatives', {layout: 'admin'});
+    }
 
-  public getRepresentativesList(req: Request, res: Response, next: NextFunction) {
-    res
-      .status(200)
-      .render('./admin/representatives', {layout: 'admin'});
-  }
-
-  public init() {
-    this.router.get('/representatives', this.getRepresentativesList)
-    this.router.get('/:id', this.updateStudent)
-    this.router.get('/', this.getStudents)
-  }
+    public init() {
+        this.router.get('/representatives', this.getRepresentativesList)
+        this.router.get('/:id', this.updateStudent)
+        this.router.get('/', this.getStudents)
+    }
 }
 
 export default new AdminStudentsRouter().router
